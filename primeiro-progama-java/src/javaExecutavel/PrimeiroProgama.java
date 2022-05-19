95f5fa7a1f086676824960fd341d0d8edefc6b18
Andrew Felipe Moreno <55249944+lipyx@users.noreply.github.com>
2022-05-19 16:26:38 -0300

    Revert "Update PrimeiroProgama.java"

--- primeiro-progama-java/src/javaExecutavel/PrimeiroProgama.java
+++ primeiro-progama-java/src/javaExecutavel/PrimeiroProgama.java
@@ -4,7 +4,7 @@ public class PrimeiroProgama {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Ol√° Mundo! primeiro progama em Java :D");
+		System.out.println("Ol· Mundo! primeiro progama em Java");
 		
 	}
 	
